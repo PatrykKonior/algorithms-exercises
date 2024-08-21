aff5ac069c318fe58ceb184afdb9b74354527717
Patryk <patrykkonior4@gmail.com>
2024-08-21 20:35:31 +0200

    Self Balancing AVL Tree Soluton

--- specs/avl/avl.test.js
+++ specs/avl/avl.test.js
@@ -47,10 +47,30 @@ class Node {
     this.height = 1;
   }
   add(value) {
-    // decide to go left or right
-
     // find the correct place to add
-    // make sure you're updating heights
+    //go left
+    if (value < this.value) {
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = new Node(value);
+      }
+      // make sure you're updating heights
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      // go right
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = new Node(value);
+      }
+      // make sure you're updating heights
+      if (!this.left || this.right.height > this.left.height) {
+        this.height = this.right.height + 1;
+      }
+    }
 
     this.balance();
   }
@@ -62,25 +82,68 @@ class Node {
     if single, just call rotate on self
     if double, call rotate on child then on self
      */
+    const rightHeight = this.right ? this.right.height : 0;
+    const leftHeight = this.left ? this.left.height : 0;
+
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left.right ? this.left.right.height : 0;
+      const leftLeftHeight = this.left.left ? this.left.left.height : 0;
+
+      // double rotation
+      if(leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR();
+      }
+
+      this.rotateLL()
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right.right ? this.right.right.height : 0;
+      const rightLeftHeight = this.right.left ? this.right.left.height : 0;
+
+      // double rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL();
+      }
+
+      this.rotateRR()
+    }
   }
   rotateRR() {
     // if the right child is heavy
     // from the right to the left
-
-    // at the end make rotation
-    this.right.updateInNewLocation();
+    const valueBefore = this.value;
+    const leftBefore = this.left;
+    this.value = this.right.value;
+    this.left = this.right;
+    this.right = this.right.right;
+    this.left.right = this.left.left;
+    this.left.left = leftBefore;
+    this.left.value = valueBefore;
+    this.left.updateInNewLocation();
     this.updateInNewLocation();
   }
   rotateLL() {
     // if the left child is heavy
     // from the left to the right
-
-    // at the end make rotation
-    this.left.updateInNewLocation();
+    const valueBefore = this.value;
+    const rightBefore = this.right;
+    this.value = this.left.value;
+    this.right = this.left;
+    this.left = this.left.left;
+    this.right.left = this.right.right;
+    this.right.right = rightBefore;
+    this.right.value = valueBefore;
+    this.right.updateInNewLocation();
     this.updateInNewLocation();
   }
   updateInNewLocation() {
     // calculate the new height
+    if (!this.right && !this.left) {
+      this.height = 1
+    } else if (!this.right || (this.left && this.right.height < this.left.height)) {
+      this.height = this.left.height + 1;
+    } else {
+      this.height = this.right.height + 1;
+    }
   }
 }
 
