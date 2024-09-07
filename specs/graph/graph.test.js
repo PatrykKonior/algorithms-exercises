11d02894211c4de90fdf95f92ef4680aefe41a2b
Patryk <patrykkonior4@gmail.com>
2024-09-07 19:41:18 +0200

    graph practice

--- specs/graph/graph.test.js
+++ specs/graph/graph.test.js
@@ -27,6 +27,44 @@ const { getUser } = require("./jobs");
 
 const findMostCommonTitle = (myId, degreesOfSeparation) => {
   // code goes here
+  let queue = [myId];
+  const seen = new Set(queue);
+  const jobs = {};
+
+  for (let i = 0; i <= degreesOfSeparation.length; i++) {
+    const newQueue = [];
+    while (queue.length) {
+      const user = getUser(queue.shift());
+
+      // queue up the next iteration
+      for (let j = 0; j < user.connections.length; j++) {
+        const connection = user.connections[j];
+        if (!seen.has(connection)) {
+          newQueue.push(connection);
+          seen.add(connection);
+        }
+      }
+
+      jobs[user.title] = jobs[user.title] ? jobs[user.title] + 1 : 1;
+    }
+
+    queue = newQueue;
+  }
+
+  const jobKeys = Object.keys(jobs);
+
+  let biggestNumber = jobs[jobKeys[0]];
+  let jobName = jobKeys[0];
+
+  for (let i = 1; i < jobKeys.length; i++) {
+    const currentJob = jobKeys[i];
+    if (jobs[currentJob] > biggestNumber) {
+      jobName = currentJob;
+      biggestNumber = jobs[currentJob];
+    }
+  }
+
+  return jobName;
 };
 
 // unit tests
